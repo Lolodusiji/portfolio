8ae738ddae1db0405ab201b51bcee394914ce43d
Lolodusiji <sholaodusiji@gmail.com>
2022-08-11 05:53:02 -0700

    third commit

--- app.js
+++ app.js
@@ -7,3 +7,26 @@ window.addEventListener('scroll',() =>{
         navbar.classList.remove('active')
     }
 })
+
+const links = document.querySelectorAll('.link')
+
+links.forEach((value, index) => {
+    value.addEventListener('click', () => {
+        links.forEach(val => {
+            val.classList.remove('active')
+        })
+        links[index].classList.add('active')
+    })
+})
+
+const icon = document.querySelector('#ham-burger')
+const list =  document.querySelector('.group2')
+
+icon.addEventListener('click',() =>{
+    list.classList.toggle('active')
+    if(icon.classList.contains('fa-bars')){
+        icon.classList.replace('fa-bars','fa-times')
+    }else{
+        icon.classList.replace('fa-times', 'fa-bars')
+    }
+})
